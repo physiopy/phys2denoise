294525edb2bf36b1dfca19a557b1e3ec2a1310c5
Taylor Salo <tsalo006@fiu.edu>
2020-05-05 19:21:02 -0400

    Add RRF.

--- phys2denoise/metrics/chest_belt.py
+++ phys2denoise/metrics/chest_belt.py
@@ -2,6 +2,7 @@
 """
 import numpy as np
 import pandas as pd
+import peakdet
 from scipy.ndimage.filters import convolve1d
 from scipy.signal import resample, detrend
 from scipy.stats import zscore
@@ -100,11 +101,58 @@ def rv(belt_ts, samplerate, out_samplerate, window=6, lags=(0,)):
     return rv_out
 
 
-def rvt(belt_ts, samplerate, out_samplerate, window=10):
+def rvt(belt_ts, samplerate, out_samplerate, window=10, lags=(0,)):
     """Respiratory volume-per-time
     """
     pass
 
+
+def rrf(samplerate, oversampling=50, time_length=32, onset=0.):
+    """
+    Calculate the respiratory response function using the definition
+    supplied in Chang and Glover (2009).
+
+    Inputs
+    ------
+    samplerate : :obj:`float`
+        Sampling rate of data, in seconds.
+    oversampling : :obj:`int`, optional
+        Temporal oversampling factor, in seconds
+    time_length : :obj:`int`, optional
+        RRF kernel length, in seconds
+    onset : :obj:`float`, optional
+        Onset of the response, in seconds
+
+    Outputs
+    -------
+    rrf: array-like
+        respiratory response function
+
+    Notes
+    -----
+    This respiratory response function was defined in [1]_, Appendix A.
+
+    The core code for this function comes from metco2, while several of the
+    parameters, including oversampling, time_length, and onset, are modeled on
+    nistats' HRF functions.
+
+    References
+    ----------
+    .. [1] C. Chang & G. H. Glover, "Relationship between respiration,
+       end-tidal CO2, and BOLD signals in resting-state fMRI," Neuroimage,
+       issue 47, vol. 4, pp. 1381-1393, 2009.
+    """
+    def _rrf(t):
+        rf = (0.6 * t ** 2.1 * np.exp(-t / 1.6) -
+              0.0023 * t ** 3.54 * np.exp(-t / 4.25))
+        return rf
+    dt = tr / oversampling
+    time_stamps = np.linspace(0, time_length,
+                              np.rint(float(time_length) / dt).astype(np.int))
+    time_stamps -= onset
+    rrf_arr = _rrf(time_stamps)
+    rrf_arr = rrf_arr / max(abs(rrf_arr))
+    return rrf_arr
 
 def rrf(samplerate):
     """Respiratory response function
