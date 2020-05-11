41df1f4abbb7a294fa3e1571897043c0d03fa064
CesarCaballeroGaudes <c.caballero@bcbl.eu>
2020-05-11 13:06:38 +0200

    implementing retroicor

---
+++ phys2denoise/metrics/retroicor.py
@@ -0,0 +1,12 @@
+"""implementation of retroicor"""
+
+import argparse as ap
+import numpy as np
+import scipy as sp
+import json
+import string
+import random
+import matplotlib as mpl; mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+from scipy.signal import argrelmax
+
