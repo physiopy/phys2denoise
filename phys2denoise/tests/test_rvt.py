a1fa4077a2d34a1fd40fd16ad621ccc21c7312a8
Ryan Lim <rzlim08@gmail.com>
2020-12-26 18:36:59 -0500

    switched to fake data

--- phys2denoise/tests/test_rvt.py
+++ phys2denoise/tests/test_rvt.py
@@ -1,26 +1,30 @@
 import peakdet
 import numpy as np
-import matplotlib.pyplot as plt
 from phys2denoise.metrics.rvt import rvt
 
 
-def read_real_phys():
-    real_physio = np.genfromtxt("../../data/sub-A00077637_ses-BAS1_task-rest_acq-1400_physio.tsv.gz")
-    phys = peakdet.Physio(real_physio[:, 2]-real_physio[:, 2].mean(), fs=62.5)
+def read_fake_phys():
+    f = 0.3
+    fs = 62.5  # sampling rate
+    t = 300
+    samples = np.arange(t * fs) / fs
+    noise = np.random.normal(0, 0.5, len(samples))
+    fake_phys = 10 * np.sin(2 * np.pi * f * samples) + noise
+    phys = peakdet.Physio(fake_phys, fs=62.5)
     phys = peakdet.operations.filter_physio(phys, cutoffs=3, method="lowpass")
     return phys
 
+
 def test_peakdet():
-    phys = read_real_phys()
+    phys = read_fake_phys()
     phys = peakdet.operations.peakfind_physio(phys)
     assert phys.troughs is not None
     assert phys.peaks is not None
 
+
 def test_rvt():
-    phys = read_real_phys()
+    phys = read_fake_phys()
     phys = peakdet.operations.peakfind_physio(phys)
     r = rvt(phys.data, phys.peaks, phys.troughs, samplerate=phys.fs)
-    plt.plot(r)
-    plt.show()
-
-
+    assert r is not None
+    assert len(r) == 18750
