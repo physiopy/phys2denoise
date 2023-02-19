a76f86f257847d0794b9adbd49da5efaa6d1cb95
smoia <s.moia.research@gmail.com>
2023-02-19 17:06:54 +0100

    feat: Start adding heart beat interval

--- phys2denoise/metrics/cardiac.py
+++ phys2denoise/metrics/cardiac.py
@@ -9,6 +9,85 @@ def iht():
     pass
 
 
+def heart_beat_interval(card, peaks, samplerate, window, central_measure="mean"):
+    """Calculate the average heart beats interval (HBI) in a sliding window.
+
+    Parameters
+    ----------
+    card : str or 1D numpy.ndarray
+        Timeseries of recorded cardiac signal
+    peaks : str or 1D numpy.ndarray
+        array of peak indexes for card.
+    samplerate : float
+        Sampling rate for card, in Hertz.
+    window : float, optional
+        Size of the sliding window, in seconds.
+        Default is 6.
+    central_measure : string
+        Measure of the center used (mean or median).
+        Default is "mean".
+    Returns
+    -------
+    hbi : 2D numpy.ndarray
+        Heart Beats Interval values.
+        The first column is raw HBI values.
+        The second column is HBI values convolved with the RRF.
+
+    Notes
+    -----
+    Heart beats interval (HBI) was introduced in [1]_, and consists of the
+    average of the time interval between two heart beats based on ppg data within
+    a 6-second window.
+
+    This metric is often lagged back and/or forward in time and convolved with
+    an inverse of the cardiac response function before being included in a GLM.
+
+    References
+    ----------
+    .. [1] J. E. Chen & L. D. Lewis, "Resting-state "physiological networks"", Neuroimage,
+        vol. 213, pp. 116707, 2020.
+    """
+    # Convert window to time points
+    size = len(card)
+    halfwindow_samples = int(round(window * samplerate / 2))
+    hbi_arr = np.empty(size)
+
+    for i in range(size):
+        if i < 120:
+            window_tp = i + halfwindow_samples
+        elif i > (size - 1 - 120):
+            window_tp = (size - 1 - i) + halfwindow_samples
+        else:
+            window_tp = window_size
+
+        condition = (card.peaks >= (i - window_tp / 2)) & (card.peaks <= (i + window_tp / 2))
+        peaks = card.peaks[condition]
+
+        if central_measure == "mean":
+            hbi_arr[i] = np.mean(np.ediff1d(peaks))
+        elif central_measure == "median":
+            hbi_arr[i] = np.median(np.ediff1d(peaks))
+        else:
+            return
+
+    hbi_arr[np.isnan(hbi_arr)] = 0.0
+
+    # Convolve with crf
+    crf_arr = crf(samplerate)
+    icrf_arr = -crf_arr  # geometric mean
+    hbi_convolved = np.convolve(hbi_arr, icrf_arr)
+    hbi_convolved = np.interp(hbi_convolved, (hbi_convolved.min(), hbi_convolved.max()), (hbi_arr.min(), hbi_arr.max()))
+    #hbi_convolved = convolve1d(hbi_arr, icrf_arr, axis=0)
+
+    # Concatenate the raw and convolved versions
+    hbi_combined = np.stack((hbi_arr, hbi_convolved), axis=-1)
+
+    # Normalize to z-score
+    hbi_combined = hbi_combined - np.mean(hbi_combined, axis=0)
+    hbi_out = zscore(hbi_combined, axis=0)
+    return hbi_out
+
+
 def cardiac_phase(peaks, sample_rate, slice_timings, n_scans, t_r):
     """Calculate cardiac phase from cardiac peaks.
 
