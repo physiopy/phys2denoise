8d88fe21ce30246c7e9321dfa89b69ef4ab8b5b1
smoia <s.moia.research@gmail.com>
2023-02-20 09:07:34 +0100

    Change function import

--- phys2denoise/metrics/__init__.py
+++ phys2denoise/metrics/__init__.py
@@ -1,11 +1,13 @@
 """Metrics derived from physiological signals."""
-from .cardiac import crf, cardiac_phase
-from .chest_belt import rpv, env, rv, rrf, respiratory_phase
+from .cardiac import cardiac_phase, heart_beat_interval
+from .chest_belt import env, respiratory_phase, rpv, rv
+from .metrics import crf, rrf
 from .multimodal import retroicor
 
 __all__ = [
     "crf",
     "cardiac_phase",
+    "heart_beat_interval",
     "rpv",
     "env",
     "rv",
